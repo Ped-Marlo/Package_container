fc72f75990bddcb9a364d619de2dcb1c94ebd57b
Pedro Martinez Lopez <61512897+Ped-Marlo@users.noreply.github.com>
2020-11-09 23:51:51 +0100

    Add files via upload

--- Utilities.py
+++ Utilities.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Wed Apr  8 19:37:06 2020
+
+@author: Pedro_Martínez
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Sun Apr  5 19:46:52 2020
 
 @author: Pedro_Martínez
@@ -13,41 +21,41 @@ Static methods don’t have access to cls or self. They work like regular functi
 """
 import pandas as pd
 import numpy as np
-import os
+import os 
 import statistics
 
-
 class Data:
-    __slots__ = ['filename', 'data']
-
-    def __init__(self, filename='DataFrame', data=pd.DataFrame()):
-        self.df_raw_data = data
+    
+    def __init__(self, filename, sep=None):
         self.filename = filename
         self.filepath = os.getcwd()
-
-    @classmethod
-    def find_nan(cls, df):
+        if filename.endswith('.csv'):
+            self.df_raw_data = pd.read_csv(os.path.join(self.filepath, filename),sep)
+        self.nan_cols = self.find_nan(self.df_raw_data)
+#        self.df_filled_na = self.replace_nan(self.df_raw_data, self.nan_cols)
+        
+    
+    def find_nan(self,df):
         nan_values = df.isna()
         nan_columns = nan_values.any()
         cols = df.columns[nan_columns].tolist()
         return cols
+    
 
-    @classmethod
-    def replace_nan(cls, df, cols):
-        df_na = df.copy()
+    def replace_nan(self, df, cols):
+        df_na=df.copy()
         for col in cols:
             try:
                 mean_col = df[col].mean()
-                df_na[col] = df[col].fillna(mean_col)
-
-            except TypeError:  # mean impossible to perferm--> categorical data
+                df_na[col]=df[col].fillna(mean_col)
+                
+            except:#mean impossible to perform--> categorical data
                 mode_col = statistics.mode(df[col])
-
+                       
                 if pd.isnull(mode_col):
-                    df_na[col] = pd.get_dummies(df[col], dummy_na=True)[np.nan]
-                    df_na[col] = ~df_na[col].astype(bool)
+                    df_na[col]= pd.get_dummies(df[col],dummy_na=True)[np.nan]
                 else:
-                    df_na[col] = df[col].fillna(mode_col)
+                    df_na[col]=df[col].fillna(mode_col)   
         return df_na
 
 
